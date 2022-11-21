593a2e9b61e78c3938ac2aa4a17c9fb4b5099194
JungMin <cvy5375@naver.com>
2022-11-21 15:58:54 +0900

    예외처리

--- hello.py
+++ hello.py
@@ -1,18 +1,19 @@
-# f-string
+# 예외처리
 
-scores = [
-    {'name':'영수','score':70},
-    {'name':'영희','score':65},
-    {'name':'기찬','score':75},
-    {'name':'희수','score':23},
-    {'name':'서경','score':99},
-    {'name':'미주','score':100},
-    {'name':'병태','score':32}    
+people = [
+    {'name': 'bob', 'age': 20},
+    {'name': 'carry', 'age': 38},
+    {'name': 'john', 'age': 7},
+    {'name': 'smith', 'age': 17},
+    {'name': 'ben', 'age': 27},
+    {'name': 'bobby'},
+    {'name': 'red', 'age': 32},
+    {'name': 'queen', 'age': 25}
 ]
 
-for s in scores:
-  name = s['name']
-  score = str(s['score']) # 숫자를 문자열로 변환
-  print(name + '의 점수는 ' + score + '점 입니다.') # f-string 없이 나타낼 때
-  print(f'{name}의 점수는 {score}점 입니다.') # f-string 적용시
-  
+for person in people:
+   try:   # 예외 구문 
+      if person['age'] > 20:
+         print(person['name'])
+   except: # try 에서 에러가 났을경우
+      print(person['name'], '에러입니다')
