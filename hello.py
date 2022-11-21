89e142b0157de0f685bf7c38c3f7523dd4eca3a4
JungMin <cvy5375@naver.com>
2022-11-21 15:39:19 +0900

    튜플, 집합

--- hello.py
+++ hello.py
@@ -1,15 +1,20 @@
-# 함수
+# 튜플
+a = [1,2,3,4,5] # 리스트 양식
+b = (1,2,3,4,5) # 튜플 양식
 
-def check_gender(pin):
-   num = pin.split('-')[1][:1]
-   num = int(num)
-   if num % 2 == 0:
-      print('여자입니다')
-   else:
-      print('남자입니다')
+# a[1] = 5 # 튜플은 불변형이라 수정이나 추가가 불가능
 
+print(a, b)   
 
+# 집합
 
-check_gender('150101-1012345')
-check_gender('150101-2012345')
-check_gender('150101-4012345')
+student_a = ['물리2','국어','수학1','음악','화학1','화학2','체육']
+student_b = ['물리1','수학1','미술','화학2','체육']
+
+set_a = set(student_a) # 중복 제거
+set_b = set(student_b) 
+
+print(set_a & set_b) # 교집합
+print(set_a | set_b) # 합집합
+print(set_a - set_b) # 차집합
+print(set_a.difference(set_b)) # 차집합
