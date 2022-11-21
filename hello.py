7fdfda3950325d94f7bab6a2e76a6a520ca9f284
JungMin <cvy5375@naver.com>
2022-11-21 14:43:24 +0900

    최대값 구하기

--- hello.py
+++ hello.py
@@ -1,19 +1,11 @@
-# 반복문
+# 최대값 구하기
 
-people = [
-    {'name': 'bob', 'age': 20},
-    {'name': 'carry', 'age': 38},
-    {'name': 'john', 'age': 7},
-    {'name': 'smith', 'age': 17},
-    {'name': 'ben', 'age': 27},
-    {'name': 'bobby', 'age': 57},
-    {'name': 'red', 'age': 32},
-    {'name': 'queen', 'age': 25}
-]
+num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
 
-for i, person in enumerate(people):
-    name = person['name']
-    age = person['age']
-    print(i, name, age)
-    if i > 5:
-        break
+max = 0
+
+for num in num_list:
+   if (max < num):
+      max = num
+
+print(max)
