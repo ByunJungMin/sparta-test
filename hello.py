24f2c315ea1459f7ec60d64fedd795c846395e35
JungMin <cvy5375@naver.com>
2022-11-21 19:35:00 +0900

    class

--- hello.py
+++ hello.py
@@ -1,19 +1,27 @@
-# 함수 인자 다루는 법
+# 클래스
 
-def cal(a,b):
-   return a + 2 * b
+class Monster():                   # class 선언
+   hp = 100
+   alive = True
 
-result = cal(a=1,b=2)   # a=1 b=2 처럼 값을 고정해서 넘겨줄 경우 순서에 상관이 없다.
-                        # ex) (b=2,a=1)  
-print(result)
-
-''''''''''''
-
-def cal(a,b=2):         # 매개변수에 값을 정해준 경우 넘겨받는 값이 없으면 기존에 설정 되어 있는 값으로 진행한다.
-   return a + 2 * b
-
-result = cal(a=1)       
-                       
-print(result)
+   def damage(self, attack):       # damage 함수
+      self.hp = self.hp - attack
+      if self.hp < 0:
+         self.alive = False
 
 
+   def status_check(self):         # status_check 함수
+      if self.alive == True:
+         print('살았다')
+      else:
+         print('죽었다')
+
+
+
+m1 = Monster()                      # m1을 인스턴스라 한다
+m1.damage(150)
+m1.status_check()
+
+m2 = Monster()                      # m2도 인스턴스
+m2.damage(90)
+m2.status_check()
