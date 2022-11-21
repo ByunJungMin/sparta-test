7aa383ba1c5539c6e49a2ee1742021086acb5227
JungMin <cvy5375@naver.com>
2022-11-21 12:24:08 +0900

    문자열 자르기

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-#참 거짓 표현
-a = ( 3 == 2)
-b = (3 > 2)
+#문자열 자르기
 
-print(a)
-print(b)
+text = 'abcdefg'
+
+result = text[:3]
+
+print(result)
+
+################################
+
+phone = '02-123-1234'
+
+result = phone.split('-')[0]
+
+print(result)
