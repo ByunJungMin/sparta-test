d1cc91b9605e8b79efe18861d82deb1eb9bc0f92
JungMin <cvy5375@naver.com>
2022-11-21 19:17:28 +0900

    map, filter, lambda

--- hello.py
+++ hello.py
@@ -1,27 +1,31 @@
-# 한줄로 줄여 쓰기
+# map, filter, lambda
 
-num = 3
+people = [
+    {'name': 'bob', 'age': 20},
+    {'name': 'carry', 'age': 38},
+    {'name': 'john', 'age': 7},
+    {'name': 'smith', 'age': 17},
+    {'name': 'ben', 'age': 27},
+    {'name': 'bobby', 'age': 57},
+    {'name': 'red', 'age': 32},
+    {'name': 'queen', 'age': 25}
+]
 
-'''   기본적인 if 문
-if num % 2 == 0:
-   result = '짝수'
-else:
-   result = '홀수'
-'''
-result = ('짝수' if num % 2 == 0 else '홀수') # 한줄로 줄여 쓰기
-
-print(f'{num}은 {result}입니다')
+def check_adult(person):                                                          # check_adult 함수
+   return ('성인' if person['age'] > 20 else '청소년')
 
 
-a_list = [1,3,2,4,1,2]
+result = map(check_adult, people)                                                 # people 을 돌면서 check_adult에 넣어라
 
-b_list = []
+print(list(result))
 
-'''   기본적인 for 문
-for a in a_list:
-   b_list.append(a*2)
-'''
+''''''''''''''''''''''''''
 
-b_list = [a*2 for a in a_list] # 한줄로 줄여 쓰기
+result = map(lambda person : ('성인' if person['age'] > 20 else '청소년'), people) # people를 돌면서 person에다 값을 넣을건데 : 을 기준으로 오른쪽에 있는 함수를 적용할거다
+                                                                                  # lambda 함수를 한줄로 줄여쓰기 위한 표현식
+print(list(result))
 
-print(b_list)
+
+result = filter(lambda person : person['age'] > 20, people)                       # filter 조건에 맞는것만 출력
+
+print(list(result))
